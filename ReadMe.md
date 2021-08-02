3dd03b18724953b9b7370ab7da73649b34ba6527
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-08-03 03:28:10 +0600

    Completed tutorial #15.

--- ReadMe.md
+++ ReadMe.md
@@ -1,9 +1,18 @@
 # Learning Log of Tutorial 1
 Learning from [this](https://www.youtube.com/playlist?list=PLSzsOkUDsvdtl3Pw48-R8lcK2oYkk40cm) tutorial of London App Brewery.
 
+
+
 ## Lesson 1-13
 Learnt the basics and installation process of Flutter.
+
+
 
 ## Lesson 14: Creating a New Flutter Project from Scratch
 1. Created a basic material widget.
 1. Created two widgets inside that widget.
+
+
+
+## Lesson 15: Scaffolding a Material App
+1. Learnt to use an appbar and a body to create a basic app, with an interesting widget named Scaffold.
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text('Hey Jude!'),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[50],
+        appBar: AppBar(
+          title: Text('Hey Jude!'),
+          backgroundColor: Colors.blueGrey[700],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/11/30/759862-beatles-white-album.jpg'),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
